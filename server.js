347d543e18b1608b3a4cba9b57b5ab91240fc34b
richardmsciaer-star <richardm.sciaer@gmail.com>
2026-05-15 13:42:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,5 @@
-const express = require('express');
-const os = require('os');
-const osUtils = require('os-utils');
-const disk = require('diskusage');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/api/status', (req, res) => {
-    disk.check('/', (err, info) => {
-        osUtils.cpuUsage((cpu) => {
-            res.json({
-                cpu: (cpu * 100).toFixed(2),
-                mem: ((1 - os.freemem() / os.totalmem()) * 100).toFixed(2),
-                diskUsed: info ? ((info.total - info.available) / 1073741824).toFixed(2) : "N/A",
-                diskTotal: info ? (info.total / 1073741824).toFixed(2) : "N/A",
-                diskPercent: info ? ((1 - info.available / info.total) * 100).toFixed(2) : "N/A"
-            });
-        });
-    });
-});
-
-module.exports = app;
+module.exports = (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Leo OS Dashboard</h1><p>Sistema operativo activo.</p>');
+};
